6254d838213d6ecd44bb48886dfc723f23f62353
hihurmuz <56685198+hihurmuz@users.noreply.github.com>
2020-08-14 23:32:06 +0300

    language checker unnecessary comment line deleted

--- client/plugins/languageChecker.js
+++ client/plugins/languageChecker.js
@@ -1,11 +1,7 @@
 import localStorage from "localStorage";
 export default function({ store, redirect, route }) {
-  //const user = localStorage.getItem("user");
-  //const loggedStatus = store.getters['isLoggedIn']
-
   const localLanguage = localStorage.getItem("fintechProgrammingLanguage");
   if (localLanguage !== "cn") {
     store.commit("setLanguage", localLanguage);
   }
-  //console.log("veri geldimi ..." + store.state.lang);
 }
