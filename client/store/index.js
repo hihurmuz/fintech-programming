6185d5e17e5fffc983e78a274ce267acb9b68a86
hihurmuz <56685198+hihurmuz@users.noreply.github.com>
2020-08-14 22:14:07 +0300

    language data name change in vuex

--- client/store/index.js
+++ client/store/index.js
@@ -1,9 +1,9 @@
 export const state = () => ({
-  language: "us"
+  lang: "tr"
 });
 
 export const mutations = {
   setLanguage(state, lang) {
-    state.language = lang;
+    state.lang = lang;
   }
 };
