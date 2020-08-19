ece797d7028aa7a1656e20496f7a613e2f3eb5b0
hihurmuz <56685198+hihurmuz@users.noreply.github.com>
2020-08-19 21:29:36 +0300

    routers added

--- client/.nuxt/router.js
+++ client/.nuxt/router.js
@@ -5,6 +5,9 @@ import scrollBehavior from './router.scrollBehavior.js'
 
 const _66b28c60 = () => interopDefault(import('..\\pages\\articles\\_id.vue' /* webpackChunkName: "pages/articles/_id" */))
 const _064a582f = () => interopDefault(import('..\\pages\\category\\_id.vue' /* webpackChunkName: "pages/category/_id" */))
+const _7587eaf2 = () => interopDefault(import('..\\pages\\footer\\_id.vue' /* webpackChunkName: "pages/footer/_id" */))
+const _09111616 = () => interopDefault(import('..\\pages\\maincategory\\_id.vue' /* webpackChunkName: "pages/maincategory/_id" */))
+const _0e8ce26e = () => interopDefault(import('..\\pages\\menu\\_id.vue' /* webpackChunkName: "pages/menu/_id" */))
 const _0e868905 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
@@ -31,6 +34,18 @@ export const routerOptions = {
     path: "/category/:id?",
     component: _064a582f,
     name: "category-id"
+  }, {
+    path: "/footer/:id?",
+    component: _7587eaf2,
+    name: "footer-id"
+  }, {
+    path: "/maincategory/:id?",
+    component: _09111616,
+    name: "maincategory-id"
+  }, {
+    path: "/menu/:id?",
+    component: _0e8ce26e,
+    name: "menu-id"
   }, {
     path: "/",
     component: _0e868905,
