e22455108dd6367049e00eb3c8f1df09d4e6b361
hihurmuz <56685198+hihurmuz@users.noreply.github.com>
2020-08-21 20:32:31 +0300

    functions that take value according to the local language option were created

--- client/plugins/functions.js
+++ client/plugins/functions.js
@@ -1,13 +1,41 @@
 const func = {
-  categoryName(category, lang) {
+  categoryName(value, lang) {
     if (lang == "tr") {
-      return category.name_tr;
+      return value.name_tr;
     } else if (lang == "cn") {
-      return category.name_cn;
+      return value.name_cn;
     } else if (lang == "es") {
-      return category.name_es;
+      return value.name_es;
     } else if (lang == "us") {
-      return category.name_us;
+      return value.name_us;
+    } else {
+      return "";
+    }
+  },
+  mainCategoryName(value, lang) {
+    if (lang == "tr") {
+      return value.title_tr;
+    } else if (lang == "cn") {
+      return value.title_cn;
+    } else if (lang == "es") {
+      return value.title_es;
+    } else if (lang == "us") {
+      return value.title_us;
+    } else {
+      return "";
+    }
+  },
+  contentByLanguage(value, lang) {
+    if (value.id) {
+      if (lang === "tr") {
+        return content_tr;
+      } else if (lang === "cn") {
+        return content_cn;
+      } else if (lang === "es") {
+        return content_es;
+      } else {
+        return content_us;
+      }
     } else {
       return "";
     }
